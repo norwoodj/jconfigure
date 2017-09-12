535c1fa35477a6c713c304cee10844e480499fb6
John Norwood <norwood.john.m@gmail.com>
2017-09-12 17:18:30 -0500

    Initial Commit

---
+++ jscripts/settings/application-settings.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+[[ -n "${_APPLICATION_SETTINGS_SH:+_}" ]] && return || readonly _APPLICATION_SETTINGS_SH=1
+
+##
+# This is where you should define the names of every application that is built by this project, an application being some
+# collection of docker containers that should run as an atomic unit. These names will be used in scripts throughout this
+# project, for instance in the "run-locally.sh" script, where you will specify one of these applications to be run
+##
+readonly WEB_APPLICATION_NAME="jscripts-web"
+
+readonly _APPLICATION_CONFIG=$(cat <<EOF
+{
+    "applications": [
+        "${WEB_APPLICATION_NAME}"
+    ],
+    "imagesToRunApp": {
+        "${WEB_APPLICATION_NAME}": ["${NGINX_IMAGE}"]
+    }
+}
+EOF
+)
+
+
+function get_app_list {
+    jq -r ".applications[]" <<< "${_APPLICATION_CONFIG}"
+}
+
+function print_app_usage_list {
+    echo "  ${WEB_APPLICATION_NAME}  A web application"
+}
+
+function get_images_necessary_to_run_app {
+    local app=${1}
+    jq -r ".imagesToRunApp.${app}[]" <<< "${_APPLICATION_CONFIG}"
+}
+
+function get_current_application_version {
+    cat version.txt
+}
---
+++ jscripts/settings/docker-settings.sh
@@ -0,0 +1,115 @@
+#!/usr/bin/env bash
+
+[[ -n "${_DOCKER_SETTINGS_SH:+_}" ]] && return || readonly _DOCKER_SETTINGS_SH=1
+
+source ${SCRIPT_DIR}/settings/common.sh
+source ${SCRIPT_DIR}/settings/application-settings.sh
+
+##
+# Environment variables that control how images are named/deployed/built etc.
+##
+: ${LOCAL_IMAGE_VERSION_TAG:="current"}
+: ${DOCKER_REGISTRY_URL:=""}
+: ${DOCKER_REGISTRY_ORG:=""}
+: ${ADDITIONAL_DOCKER_BUILD_ARGS:=""}
+: ${ADDITIONAL_DOCKER_PUSH_ARGS:=""}
+: ${DOCKER_PUSH_KEEP_TAGGED:="false"}
+
+
+##
+# List here all of the images built/deployed/used by the project
+##
+readonly NGINX_IMAGE="nginx"
+
+readonly _DOCKER_CONFIG=$(cat <<EOF
+{
+    "buildImages": [
+        "${NGINX_IMAGE}"
+    ],
+    "deployImages": [
+        "${NGINX_IMAGE}"
+    ],
+    "imageDependencies": {}
+}
+EOF
+)
+
+
+##
+# Settings for which images should be built/deployed by these scripts, as well as how to build and deploy them
+##
+function print_build_images_usage_list {
+    echo "  $(join "\n  " $(get_images_to_build))"
+}
+
+function print_deploy_images_usage_list {
+    echo "  $(join "\n  " $(get_images_to_deploy))"
+}
+
+function get_images_to_build {
+    jq -r ".buildImages[]" <<< "${_DOCKER_CONFIG}"
+}
+
+function get_images_to_deploy {
+    jq -r ".deployImages[]" <<< "${_DOCKER_CONFIG}"
+}
+
+function get_image_dependencies_for_image {
+    local image=${1}
+    jq -r ".imageDependencies.${image} | if . == null then \"\" else .[] end" <<< "${_DOCKER_CONFIG}"
+}
+
+function get_image_name {
+    local image=${1}
+    echo "${PROJECT_NAME}-${image}"
+}
+
+function get_dockerfile_path_for_image {
+    local image=${1}
+    echo "docker/Dockerfile-${image}"
+}
+
+function get_docker_build_context_path_for_image {
+    local image=${1}
+    echo "."
+}
+
+function get_additional_docker_build_args {
+    local image=${1}
+}
+
+function get_image_version {
+    local image=${1}
+    get_current_application_version
+}
+
+function get_docker_registry_name {
+    if [[ -z "${DOCKER_REGISTRY_URL:+_}" ]]; then
+        echo "DockerHub"
+    else
+        echo "${DOCKER_REGISTRY_URL:+_}"
+    fi
+}
+
+##
+# Hooks around building and deploying images
+##
+function pre_build_image_hook {
+    local image=${1}
+    log_debug "Pre Build Image Hook for image "${image}""
+}
+
+function post_build_image_hook {
+    local image=${1}
+    log_debug "Post Build Image Hook for image "${image}""
+}
+
+function pre_deploy_image_hook {
+    local image=${1}
+    log_debug "Pre Deploy Image Hook for image "${image}""
+}
+
+function post_deploy_image_hook {
+    local image=${1}
+    log_debug "Post Deploy Image Hook for image "${image}""
+}

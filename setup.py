ee6733ee839d2def76a5019fe8a56eb8395ddad7
John Norwood <norwood.john.m@gmail.com>
2018-08-01 21:01:34 +0000

    [RELEASE] Bumping versions to next development version 18.0802-dev

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name="jconfigure",
     packages=["jconfigure"],
-    version="18.0801.1",
+    version="18.0802-dev",
     description="A python configuration management library",
     author="John Norwood",
     author_email="norwood.john.m@gmail.com",
     url="https://github.com/norwoodj/jconfigure",
-    download_url="https://github.com/norwoodj/jconfigure/archive/18.0801.1.tar.gz",
+    download_url="https://github.com/norwoodj/jconfigure/archive/18.0802-dev.tar.gz",
     keywords=["configuration"],
     classifiers=[],
     install_requires=[

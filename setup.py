d5ae91d8d2c9923d33ff7e3099fa9b056a02e1fb
John Norwood <norwood.john.m@gmail.com>
2019-01-22 16:46:39 +0000

    [RELEASE] Bumping versions to next development version 19.0123-dev

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name="jconfigure",
     packages=["jconfigure"],
-    version="19.0122",
+    version="19.0123-dev",
     description="A python configuration management library",
     author="John Norwood",
     author_email="norwood.john.m@gmail.com",
     url="https://github.com/norwoodj/jconfigure",
-    download_url="https://github.com/norwoodj/jconfigure/archive/19.0122.tar.gz",
+    download_url="https://github.com/norwoodj/jconfigure/archive/19.0123-dev.tar.gz",
     keywords=["configuration"],
     classifiers=[],
     install_requires=[
